48f12e7b5fe733ba1c4cebe7c0d89446e605cc9f
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-25 11:53:11 +0530

    Add files via upload

---
+++ Ip & Op/06.py
@@ -0,0 +1,29 @@
+# Question;-
+'''
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+First-line indicates two integers separated by space. Second-line indicates three integers separated by space. Third-line indicates three integers separated by space
+
+Output Description:
+Print the input in the same format.
+
+Sample Input :
+2 5
+2 5 6
+2 4 5
+Sample Output :
+2 5
+2 5 6
+2 4 5
+'''
+
+
+
+# Solution:
+l = input()
+m = input()
+n = input()
+print(l)
+print(m)
+print(n)
