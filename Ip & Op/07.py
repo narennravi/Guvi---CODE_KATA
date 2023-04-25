6919c3fda3a36ee094d60abe36d4a7f22effca63
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-25 11:53:28 +0530

    Add files via upload

---
+++ Ip & Op/07.py
@@ -0,0 +1,23 @@
+# Question:-
+'''
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+A single line contains a string.
+
+Output Description:
+Print the characters in a string separated by space.
+
+Sample Input :
+guvi
+Sample Output :
+g u v i
+'''
+
+
+# Solution:-
+name = input()
+
+for i in name[:len(name)-1]:
+    print(i, end=' ')
+print(name[len(name)-1])
