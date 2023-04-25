7423b2a52fd112591721bcd73060a1b96bb39635
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-25 11:52:13 +0530

    Add files via upload

---
+++ Ip & Op/03.py
@@ -0,0 +1,26 @@
+# Question:
+'''
+Write a code to get the input in the given format and print the output in the given format.
+
+Input Description:
+First-line indicates two integers which are the size of array and 'K' value. Second-line indicates an integer contains elements of an array.
+
+Output Description:
+Print the taken input in the same format.
+
+Sample Input :
+5 3
+1 2 3 4 5
+Sample Output :
+5 3
+1 2 3 4 5
+'''
+
+
+
+
+# Solution:
+soa = input()
+k_val = input()
+print(soa)
+print(k_val)
