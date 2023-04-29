5a9eb27cb8e2cab458afc444b63c7f39f66cdef7
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-29 18:32:48 +0530

    Add files via upload

---
+++ Ab_Beginner/13.py
@@ -0,0 +1,22 @@
+# Question:-
+'''
+Let "A"  be a string. Remove all the whitespaces and find it's length.
+
+Input Description:
+A string is provide as an input
+
+Output Description:
+Remove all the whitespaces and then print the length of the remaining string.
+
+Sample Input :
+Lorem Ipsum
+Sample Output :
+10
+'''
+
+
+
+# Solution:-
+A = input()
+A.replace(" ","")
+print(len(A.replace(" ","")))
