2600d1bcd6887a518cb7862d235aadcc700db315
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-29 18:34:39 +0530

    Add files via upload

---
+++ Ab_Beginner/18.py
@@ -0,0 +1,26 @@
+# Question:-
+'''
+Write a code get an integer number as input and print the sum of the digits.
+
+Input Description:
+A single line containing an integer.
+
+Output Description:
+Print the sum of the digits of the integer.
+
+Sample Input :
+124
+Sample Output :
+7
+'''
+
+
+
+# Solution:-
+n = int(input())
+total = 0
+while n > 0:
+    dig = n % 10
+    total = total + dig
+    n = n // 10
+print(total)
