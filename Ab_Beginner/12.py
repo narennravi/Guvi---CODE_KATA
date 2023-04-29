5c1ca55fe7327b4b67d9e82a7b49b1f8ce1677fb
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-29 18:32:35 +0530

    Add files via upload

---
+++ Ab_Beginner/12.py
@@ -0,0 +1,21 @@
+# Question:-
+'''
+You are given with a number "N", find its cube.
+
+Input Description:
+A positive integer is provided.
+
+Output Description:
+Find the cube of the number.
+
+Sample Input :
+2
+Sample Output :
+8
+'''
+
+
+
+# Solution:-
+n = int(input())
+print(n**3)
