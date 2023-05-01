47929924cebd582b51fdfb12e1632c4b2137178f
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-01 20:26:59 +0530

    Add files via upload

---
+++ Basic/02.py
@@ -0,0 +1,21 @@
+# Question:-
+'''
+Given 2 numbers N and K followed by elements of N .Print 'yes' if K exists else print 'no'.
+Sample Testcase :
+INPUT
+4 2
+1 2 3 3
+OUTPUT
+yes
+'''
+
+
+
+# Solution:-
+a,b = map(int,input().split())
+arr = list(map(int,input().split()))
+
+if b in arr:
+    print("yes")
+else:
+    print("no")
