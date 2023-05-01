f64a1b25d07160d08cdf98a5ff67c5988f258740
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-01 20:29:29 +0530

    Add files via upload

---
+++ Basic/09.py
@@ -0,0 +1,18 @@
+# Question:-
+'''
+Write a program to print the sum of the first K natural numbers.
+Input Size : n <= 100000
+Sample Testcase :
+INPUT
+3
+OUTPUT
+6
+'''
+
+
+# Solution:-
+a = int(input())
+sum = 0
+for i in range(a+1):
+    sum += i
+print(sum)
