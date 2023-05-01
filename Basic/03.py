196819a7a3dfd837f5ef305be4f4697d7b5dd3e2
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-01 20:27:21 +0530

    Add files via upload

---
+++ Basic/03.py
@@ -0,0 +1,17 @@
+# Question:-
+'''
+Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
+Sample Testcase :
+INPUT
+5 5
+OUTPUT
+yes
+'''
+
+
+# Solution:-
+N,M=input().split()
+if N==M:
+  print('yes')
+else:
+  print('no')
