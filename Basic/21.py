c83452b1ea8a2fc756ec1d1536b3c231a9f8ac66
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-03 22:10:00 +0530

    Add files via upload

---
+++ Basic/21.py
@@ -0,0 +1,18 @@
+# Question:-
+'''
+Given 3 numbers A,B,C print 'yes' if they can form the sides of a right angled triangle,otherwise 'no'.
+Input Size : A,B,C <= 100000
+Sample Testcase :
+INPUT
+3 4 5
+OUTPUT
+yes
+'''
+
+
+# Solution:-
+A,B,C=(int(no) for no in input().split())
+if (A * A)+(B * B)==C*C:
+  print('yes')
+else:
+  print('no')
