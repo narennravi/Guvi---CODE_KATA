cbc057b6f10db81e342432d1c11b4da29c56c782
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-08 22:30:34 +0530

    Add files via upload

---
+++ Loop/09.py
@@ -0,0 +1,25 @@
+# Question:-
+'''
+Write a code to get an integer N and print the even values from 1 till N in a separate line.
+
+Input Description:
+A single line contains an integer N.
+
+Output Description:
+Print the even values from 1 to N in a separate line.
+
+Sample Input :
+6
+Sample Output :
+2
+4
+6
+'''
+
+
+
+# Solution:-
+n = int(input())
+for i in range(1,n+1):
+    if i%2==0:
+        print(i)
