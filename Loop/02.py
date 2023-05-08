4c778ae9486e1fb07e48bb3ba22a9ce0468cca59
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-08 22:27:25 +0530

    Add files via upload

---
+++ Loop/02.py
@@ -0,0 +1,28 @@
+# Question:-
+'''
+Write a code to get the input and print it 5 times.
+
+Input Description:
+A single line contains an integer N.
+
+Output Description:
+Output contains 5 lines with each line having the value N.
+
+Sample Input :
+4
+Sample Output :
+4
+4
+4
+4
+4
+'''
+
+
+
+# Solution:-
+num = int(input())
+i=0
+while i<5:
+    print(num)
+    i+=1
