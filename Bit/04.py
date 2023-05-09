d1ee52b48b29fb9a41f00b0c3a780d845fc8de92
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-09 22:19:13 +0530

    Add files via upload

---
+++ Bit/04.py
@@ -0,0 +1,16 @@
+# Question:-
+'''
+Given 2 numbers N,K print the number after performing bitwise left shift 'K' times.
+Input Size : 1 <= N, K <= 1000
+Sample Testcase :
+INPUT
+5 2
+OUTPUT
+20
+'''
+
+
+
+# Solution:-
+(n, k) = map(int, input().split(" "))
+print(n << k)
