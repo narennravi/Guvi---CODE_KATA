127c33f49d32de2c0ce4efb16fdebdcab6a136be
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-09 22:20:10 +0530

    Add files via upload

---
+++ Bit/07.py
@@ -0,0 +1,17 @@
+# Question:-
+'''
+Given a number N, print the Bitwise NOT of that number.
+Input Size : 1 <= N <= 10000
+Sample Testcase :
+INPUT
+5
+OUTPUT
+-6
+'''
+
+
+
+# Solution:-
+a = int(input())
+b = ~a
+print(b)
