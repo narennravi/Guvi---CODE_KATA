cae1b64216c64ec96299c1a8f970649c5e08cd10
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-09 22:18:47 +0530

    Add files via upload

---
+++ Bit/03.py
@@ -0,0 +1,14 @@
+# Question:-
+'''
+Given a binary number convert it to hexadecimal.
+Sample Testcase :
+INPUT
+1100100
+OUTPUT
+64
+'''
+
+
+# Solution:-
+binary = int(input(), 2)
+print(hex(binary)[2:])
