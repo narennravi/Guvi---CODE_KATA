3e0aebe75b561fc563a8ddc339f408166ce9339c
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-10 22:22:51 +0530

    Add files via upload

---
+++ Bit/09.py
@@ -0,0 +1,24 @@
+# Question:-
+'''
+You are given a number ‘n’ you have to tell whether number is divisible by 8.
+
+Print 1 if it is divisible and 0 if it is not.
+
+Input Description:
+You are given a number ‘n’.
+
+Output Description:
+Print 1 if number is divisible by 8 and 0 if not
+
+Sample Input :
+16
+Sample Output :
+1
+'''
+
+
+
+# Solution:-
+n = int(input())
+if n % 8 == 0: print(1)
+else: print(0)
