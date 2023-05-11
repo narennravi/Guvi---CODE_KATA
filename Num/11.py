6211e444a71b04eff481d17992a7c3a501cef647
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-11 20:08:05 +0530

    Add files via upload

---
+++ Num/11.py
@@ -0,0 +1,20 @@
+# Question:-
+'''
+In a garage the service man takes 10 minutes to service one car.If there are N cars in garage and X is number of minutes after which one person arrives,Calculate how much time last person has to wait in garage.(Print answer in minutes)
+
+Input Description:
+You are given Two numbers ‘N’ and ‘X’
+
+Output Description:
+Waiting time of last person
+
+Sample Input :
+4 5
+Sample Output :
+15
+'''
+
+
+# Solution;-
+x,y = map(int,input().split())
+print((x-1)*10-(x-1)*y)
