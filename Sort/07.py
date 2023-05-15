3f15095bb0f7551eb4dd9cde8499c35c70fd04d3
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-15 11:50:39 +0530

    Update 07.py

--- Sort/07.py
+++ Sort/07.py
@@ -25,3 +25,14 @@ Sample Output :
 
 
 # Solution:-
+n = int(input())
+a = list(map(int,input().split(" ")))
+b = int(input())
+c = list(map(int,input().split(" ")))
+output = []
+for i in c:
+    if i in a:
+        output.append(a.count(i))
+    else:
+        output.append("Not Present")
+print(*output)
