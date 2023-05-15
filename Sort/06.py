72fec8552afab3c6dbc2613657d24a4a1788e295
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-15 11:50:05 +0530

    Update 06.py

--- Sort/06.py
+++ Sort/06.py
@@ -25,3 +25,13 @@ Rohan
 
 
 # Solution:-
+n = int(input())
+s = str(input()).split()
+a = {}
+b = []
+for i in range(0, n*2, 2):
+    a[s[i+1]] = s[i]
+    b.append(s[i+1])
+b.sort()
+for i in b:
+    print(a[i])
