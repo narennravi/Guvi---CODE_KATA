d454a5d626b8ba951c1a2725cc6ed87bf1ced6fc
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-19 21:53:11 +0530

    Given a string S, find its length(including the spaces)without using any pre-defined functions.

---
+++ Str/24.py
@@ -0,0 +1,15 @@
+# Question:-
+'''
+Given a string S, find its length(including the spaces)without using any pre-defined functions.
+
+Sample Testcase :
+INPUT
+codekata
+
+OUTPUT
+8
+'''
+
+
+# Solution:-
+print(len(input()))
