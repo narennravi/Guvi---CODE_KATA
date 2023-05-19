3a20403da6be3830aeb235fdec99c929ecb65555
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-19 21:57:58 +0530

    You are given a string. You have to print “Wonder” if the string is wonderful and -1 if it is not.

--- Str/01.py
+++ Str/01.py
@@ -1,6 +1,7 @@
 # Question:-
 '''
-You are given a string. You have to print “Wonder” if the string is wonderful and -1 if it is not. A wonderful string is a string,which is made up of exactly 3 different characters.
+You are given a string. You have to print “Wonder” if the string is wonderful and -1 if it is not. 
+A wonderful string is a string,which is made up of exactly 3 different characters.
 
 Input Description:
 You are given a string
@@ -10,6 +11,7 @@ Print “Wonder” if it is wonderful and -1 if it is not
 
 Sample Input :
 aabbcc
+
 Sample Output :
 Wonder
 '''
