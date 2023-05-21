74f5636028eb8137acdbcba778041fb50c4af411
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-05-21 17:53:15 +0530

    Given 3 numbers N , L and R. Print 'yes' if N is between L and R else print 'no'.

---
+++ Maths/17.py
@@ -0,0 +1,21 @@
+# Question:-
+'''
+Given 3 numbers N , L and R. Print 'yes' if N is between L and R else print 'no'.
+
+Sample Testcase :
+INPUT
+3
+2 6
+
+OUTPUT
+yes
+'''
+
+
+# Solution:-
+N = int(input())
+L,R = map(int,input().split())
+if N in range(L,R):
+    print("yes")
+else:
+    print("no")
