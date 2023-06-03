090ff9b030dad08c2d7907cab71ae2da56aab043
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-03 22:16:55 +0530

    Given base(B) and height(H) of a triangle find its area.

---
+++ Maths/49.py
@@ -0,0 +1,17 @@
+# Question:-
+'''
+Given base(B) and height(H) of a triangle find its area.
+Input Size : N <= 1000000
+
+Sample Testcase :
+INPUT
+2 4
+
+OUTPUT
+4
+'''
+
+
+# Solution:-
+b,h = map(int,input().split(" "))
+print(0.5*b*h)
