385c306a7d749fcf8ca7ca008e67b2423cebb7a1
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-09 20:29:17 +0530

    Add files via upload

---
+++ Basic/13.py
@@ -0,0 +1,28 @@
+# Question:-
+'''
+Write a code to get an integer N and print the sum of  values from 1 to N.
+
+Input Description:
+A single line contains an integer N.
+
+Output Description:
+Print the sum of values from 1 to N.
+
+Sample Input :
+10
+Sample Output :
+55
+'''
+
+
+
+# Solution:-
+num = int(input())
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   while num > 0:
+       sum =sum + num
+       num =num - 1
+   print(sum)
---
+++ Basic/19.py
@@ -0,0 +1,26 @@
+# Question:-
+'''
+Write a code to get a integer n as input and calculate the smallest perfect power of 2 greater than n.
+
+Input Description:
+A single line containing an integer,n.
+
+Output Description:
+Print the smallest perfect power of 2 greater than n.
+
+Sample Input :
+48
+Sample Output :
+64
+'''
+
+
+
+# Solution:-
+n=int(input())
+N=1
+
+while n>0:
+  N=N<<1
+  n=n>>1
+print(N)
