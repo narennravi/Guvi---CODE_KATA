ad0969e3bd1ffb985b1935eddd6faf38b6d89ea9
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-12 18:25:20 +0530

    Given 3 numbers A,B,C print 'yes' if they can form the sides of a scalene triangle else print 'no'.

--- Basic/20.py
+++ Basic/20.py
@@ -2,9 +2,11 @@
 '''
 Given 3 numbers A,B,C print 'yes' if they can form the sides of a scalene triangle else print 'no'.
 Input Size : A,B,C <= 100000
+
 Sample Testcase :
 INPUT
 3 4 5
+
 OUTPUT
 yes
 '''
