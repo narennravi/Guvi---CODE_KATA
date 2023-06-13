410fe3a18b32336a9ccd7c234d5a5939e33a1037
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-13 21:50:51 +0530

    Write a code get an integer number as input and print the odd and even digits of the number separately.

---
+++ Basic/30.py
@@ -0,0 +1,31 @@
+# Question:-
+'''
+Write a code get an integer number as input and print the odd and even digits of the number separately.
+
+Input Description:
+A single line containing an integer.
+
+Output Description:
+Print the even and odd integers of the integer in a separate line.
+
+Sample Input :
+1234
+
+Sample Output :
+2 4
+1 3
+'''
+
+
+# Solution:-
+num = int(input())
+even = []
+odd = []
+b = list(map(int, str(num)))
+for i in b:
+   if i%2==0:
+      even.append(i)
+   else:
+      odd.append(i)
+print(*sorted(even))
+print(*sorted(odd))
