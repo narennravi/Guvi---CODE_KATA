a2ece8d0a4d561ba21483aabd145d175a8950543
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-13 21:11:31 +0530

    Add files via upload

---
+++ Basic/23.py
@@ -0,0 +1,22 @@
+# Question:-
+'''
+Given a number N and an array of N elements, find the Bitwise OR of the array elements.
+Input Size : N <= 100000
+Sample Testcase :
+INPUT
+2
+2 4
+OUTPUT
+6
+'''
+
+
+# Solution:-
+a = int(input())
+arr = list(map(int,input().split()))
+ans = arr[0]
+
+for i in range(1, len(arr)):
+    ans = ans | arr[i]
+print(ans)
+
---
+++ Basic/24.py
@@ -0,0 +1,16 @@
+# Question:-
+'''
+Given a number N, print the Bitwise NOT of that number.
+Input Size : 1 <= N <= 10000
+Sample Testcase :
+INPUT
+5
+OUTPUT
+-6
+'''
+
+
+# Solution:-
+a = int(input())
+b = ~a
+print(b)
