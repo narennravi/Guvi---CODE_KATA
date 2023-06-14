1fc78d527b8001b5260a0d392e36424929bf732f
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-14 22:31:38 +0530

    Given a string 'S' swap the even and odd characters starting from index 1(Assume the index starts from 0).

---
+++ Basic/34.py
@@ -0,0 +1,20 @@
+# Question:-
+'''
+Given a string 'S' swap the even and odd characters starting from index 1(Assume the index starts from 0).
+Input Size : |s| <= 10000000(complexity O(n))
+
+Sample Testcase :
+INPUT
+codekata
+
+OUTPUT
+ocedakat
+'''
+
+
+# Solution:-
+S=input()
+t=list(S)
+t[::2],t[1::2]=t[1::2],t[::2]
+res=''.join(t)
+print(res)
