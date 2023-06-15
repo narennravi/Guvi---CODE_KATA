be26b4da1aeaf3b52ba610ae745af46407a32d60
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-15 14:01:57 +0530

    you are given with ‘arasu’ series(shown in example).You have to understand it and you will be given a number ‘n’ , you have to print the series till n numbers.

--- Num/01.py
+++ Num/01.py
@@ -1,6 +1,7 @@
 # Question:-
 '''
-you are given with ‘arasu’ series(shown in example).You have to understand it and you will be given a number ‘n’ ,you have to print the series till n numbers.
+you are given with ‘arasu’ series(shown in example).You have to understand it and you will be given a number ‘n’ ,
+you have to print the series till n numbers.
 
 
 Input Description:
@@ -11,11 +12,10 @@ Print series till nth number
 
 Sample Input :
 4
+
 Sample Output :
 2 5 10 17
 '''
-
-
 
 
 
