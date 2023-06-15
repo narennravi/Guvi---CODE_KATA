4410a10023288df59b33a78bac07333d4af27527
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-15 14:06:36 +0530

    Assume that you are ticket verifier at a club. Your club has decided to give a special discount to the person(s) who are satisfying the following condition  Condition:- If ticket number is divisible by date of month. You are eligible for a discount.

--- Num/14.py
+++ Num/14.py
@@ -2,9 +2,7 @@
 '''
 Assume that you are ticket verifier at a club. Your club has decided to give a special discount to the person(s) who are satisfying the following condition
 
-
 Condition:-
-
 If ticket number is divisible by date of month. You are eligible for a discount.
 
 Input Description:
@@ -17,6 +15,7 @@ Sample Input :
 6
 112 139 165 175 262 130
 22
+
 Sample Output :
 0 0 0 0 0 0
 '''
