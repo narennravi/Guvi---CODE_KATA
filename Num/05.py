7e91d09954905c9b01c0a388454fc6af6d64ef05
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-15 14:02:51 +0530

    Prateek finds it difficult to judge the minimum element in the list of elements given to him.  Your task is to develop the algorithm in order to find the minimum element.

--- Num/05.py
+++ Num/05.py
@@ -1,6 +1,7 @@
 # Question:-
 '''
-Prateek finds it difficult to judge the minimum element in the list of elements given to him. Your task is to develop the algorithm in order to find the minimum element.
+Prateek finds it difficult to judge the minimum element in the list of elements given to him. 
+Your task is to develop the algorithm in order to find the minimum element.
 
 Input Description:
 You are given ‘n’ number of elements. Next line contains n space separated numbers.
@@ -11,6 +12,7 @@ Print the minimum element
 Sample Input :
 5
 3 4 9 1 6
+
 Sample Output :
 1
 '''
