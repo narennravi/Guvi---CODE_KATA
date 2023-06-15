dc68975e484082d308e4da3592e50d9d5e3cee0b
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-15 14:05:19 +0530

    you are given with array of numbers.you have to find whether array is beautiful or not.  A beautiful array is an array whose sum of all numbers is divisible by 2, 3 and 5

--- Num/10.py
+++ Num/10.py
@@ -1,6 +1,7 @@
 # Question:-
 '''
-you are given with array of numbers.you have to find whether array is beautiful or not. A beautiful array is an array whose sum of all numbers is divisible by 2, 3 and 5
+you are given with array of numbers.you have to find whether array is beautiful or not. 
+A beautiful array is an array whose sum of all numbers is divisible by 2, 3 and 5
 
 Input Description:
 You are given a number ‘n’ denoting the size of array.Next line contains n space separated numbers.
@@ -11,6 +12,7 @@ Print 1 if array is beautiful and 0 if it is not
 Sample Input :
 5
 5 25 35 -5 30
+
 Sample Output :
 1
 '''
