949f3bc6e20117dab3727e8910b4919d171c8f7b
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-15 16:46:10 +0530

    You are a software engineer at an MNC. You are given the task of sorting the employees in your company based on their salary. Perform the task so that the employees, including yourself, will get a bonus from the management.

--- Sort/06.py
+++ Sort/06.py
@@ -8,6 +8,7 @@ CONSTRAINT:
 0<=salary<=1000000
 
 
+
 Input Description:
 Number of employees followed by their name and salary
 
@@ -17,6 +18,7 @@ Sorted list of employee names
 Sample Input :
 3
 Karthik 23000 rohan 81734 varshini 12343
+
 Sample Output :
 varshini
 Karthik
