6369b8be6d38a3ed8a0de6879f38904bb83051e7
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-15 14:08:20 +0530

    Ramesh is given  a task to generalise the array. An array is called generalise if median of that array is equal to sum ‘k’. Ramesh has less knowledge amongst median so he decided to take help from you. Your task is to count the number of elements that you must add to the median of given array equal to a number ‘k’.

--- Num/18.py
+++ Num/18.py
@@ -1,6 +1,8 @@
 # Question:-
 '''
-Ramesh is given  a task to generalise the array. An array is called generalise if median of that array is equal to sum ‘k’.Ramesh has less knowledge amongst median so he decided to take help from you.Your task is to count the number of elements that you must add to the median of given array equal to a number ‘k’.
+Ramesh is given  a task to generalise the array. An array is called generalise if median of that array is equal to sum ‘k’.
+Ramesh has less knowledge amongst median so he decided to take help from you.
+Your task is to count the number of elements that you must add to the median of given array equal to a number ‘k’.
 
 Input Description:
 First line contains a number ‘n’.next line contains n space separated numbers.next line contains a number ‘k’
@@ -12,6 +14,9 @@ Sample Input :
 6
 10 20 30 100 150 200
 30
+
+Sample Output :
+1
 '''
 
 
