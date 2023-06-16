52e369b0239bd3a80e9b29d7edd053927a26c5d8
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-16 22:48:32 +0530

    Given an array arr[] and a number K where K is smaller than the size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are NOT distinct.

--- Search/21.py
+++ Search/21.py
@@ -3,23 +3,21 @@
 Given an array arr[] and a number K where K is smaller than the size of array, the task is to find the Kth smallest element in the given array.
 It is given that all array elements are NOT distinct.
 
+
 Constraints:
 
 1 <= T <= 100
-
 1 <= N <= 105
-
 1 <= arr[i] <= 105
-
 1 <= K <= N
 
  
-
 Input Description:
 The first line of input contains an integer T, denoting the number of test cases. Then T test cases follow. Each test case consists of three lines. First line of each test case contains an integer N denoting size of the array. Second line contains N space separated integer denoting elements of the array. Third line of the test case contains an integer K.
 
 Output Description:
 Corresponding to each test case, print the desired output in a new line.
+
 
 Sample Input :
 2
