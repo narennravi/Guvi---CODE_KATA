e638ea62281087b3be705e351eb4273af7c91112
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-16 22:41:13 +0530

    Jaspreet is a marketing manager and on inspecting the product ids of products documented as ‘sold’, he finds out that the sales person has repeated a few of the product ids. He decides to warn the sales person who has repeated the same product id thrice. Your task is to find out the number of triplets(ids occurring thrice) of product ids recorded.

--- Search/10.py
+++ Search/10.py
@@ -3,13 +3,10 @@
 Jaspreet is a marketing manager and on inspecting the product ids of products documented as ‘sold’, he finds out that the sales person has repeated a few of the product ids.
 He decides to warn the sales person who has repeated the same product id thrice. Your task is to find out the number of triplets(ids occurring thrice) of product ids recorded.
 
+
 Constraints:
-
 1 <= N <= 100
-
 1 <= A[] <= 1000
-
-
 
 Input Description:
 Number of product ids followed by the list of product ids
@@ -17,9 +14,11 @@ Number of product ids followed by the list of product ids
 Output Description:
 Number of triplets detected.
 
+
 Sample Input :
 10
 2 1 1 3 1 8 7 2 2 6
+
 Sample Output :
 2
 '''
