418cb8b815b496af35b1ae7d3908793005c45c86
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-17 22:16:24 +0530

    Given a binary number convert it to hexadecimal.

---
+++ Str/55.py
@@ -0,0 +1,18 @@
+# Question:-
+'''
+Given a binary number convert it to hexadecimal.
+
+
+Sample Testcase :
+INPUT
+1100100
+
+OUTPUT
+64
+'''
+
+
+
+# Solution:-
+binary = int(input(), 2)
+print(hex(binary)[2:])
