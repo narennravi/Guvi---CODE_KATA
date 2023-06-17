a0479073f83dfa38204ce33a6022af8fcb42fb76
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-17 22:10:15 +0530

    Given a string S, print the encoded string by adding 3 to each character(a maps to d,b maps to e,c maps to f and so on). Input Size : 1 <= N <= 100000

--- Str/38.py
+++ Str/38.py
@@ -3,12 +3,14 @@
 Given a string S, print the encoded string by adding 3 to each character(a maps to d,b maps to e,c maps to f and so on).
 Input Size : 1 <= N <= 100000
 
-Sample Testcase :
-INPUT
-RADAR
 
-OUTPUT
-UDGDU
+Sample Testcase :
+
+INPUT:-
+ RADAR
+
+OUTPUT:-
+ UDGDU
 '''
 
 
