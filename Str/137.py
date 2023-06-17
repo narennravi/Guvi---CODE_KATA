7b35b8ca983f877a71b2c72adab480a96d091955
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-17 22:24:35 +0530

    Given a string S consisting of 2 words reverse the order of two words . Input Size : |S| <= 10000000

---
+++ Str/137.py
@@ -0,0 +1,21 @@
+# Question:-
+'''
+Given a string S consisting of 2 words reverse the order of two words .
+Input Size : |S| <= 10000000
+
+
+Sample Testcase :
+INPUT:
+hello world
+
+OUTPUT:
+world hello
+
+'''
+
+
+
+# Solution:-
+n = list(map(str,input().split()))
+n.reverse()
+print(*n)
