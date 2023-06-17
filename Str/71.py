38ae09de2a6b89cca94ce1063a5234e0ca264180
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-17 22:21:11 +0530

    Given a number N, print the sum of the squares of its digits. Input Size : 1 <= N <= 1000000000000000000

---
+++ Str/71.py
@@ -0,0 +1,21 @@
+# Question:-
+'''
+Given a number N, print the sum of the squares of its digits.
+Input Size : 1 <= N <= 1000000000000000000
+
+Sample Testcase :-
+INPUT:
+19
+
+OUTPUT:
+82
+'''
+
+
+# Solution:-
+num = int(input())
+
+def sum_of_squares_of_digits(num):
+    return sum(map(lambda x: int(x) ** 2, str(num)))
+
+print(sum_of_squares_of_digits(num))
