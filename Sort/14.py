3a6266f463135e28aadb55ca32da7a318b64ba27
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-19 22:14:04 +0530

    Mr.Stark wants to order the employee ids, which are recorded in a 2D matrix, in ascending order.  He wants to do it so as to allot a new id to a person who joins as a fresher.  You are the CTO of the Stark industries and you are asked by Mr.Stark to sort the data.

---
+++ Sort/14.py
@@ -0,0 +1,44 @@
+# Question:-
+'''
+Mr.Stark wants to order the employee ids, which are recorded in a 2D matrix, in ascending order. 
+He wants to do it so as to allot a new id to a person who joins as a fresher. 
+You are the CTO of the Stark industries and you are asked by Mr.Stark to sort the data.
+ 
+
+Input Description:
+Dimensions of the matrix m and n, followed by the elements of the matrix.
+
+Output Description:
+Matrix sorted in ascending order
+
+
+Sample Input :
+3 3
+87 21 34
+89 32 78
+12 23 45
+
+Sample Output :
+12 21 23
+32 34 45
+78 87 89
+'''
+
+
+
+# SOLUTION:-
+m, n = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(m)]
+
+# Flatten the matrix
+flattened = [element for row in matrix for element in row]
+
+# Sort the flattened list
+sorted_list = sorted(flattened)
+
+# Reshape the sorted list into a matrix
+sorted_matrix = [sorted_list[i:i+n] for i in range(0, len(sorted_list), n)]
+
+# Print the sorted matrix
+for row in sorted_matrix:
+    print(' '.join(map(str, row)))
