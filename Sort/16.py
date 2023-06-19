36804e56492119a85d42a2604b6e5018095b58fb
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-19 22:15:54 +0530

    Iron Man wants to extract an infinity stone from a safe. The safe is protected by a password and Iron Man knows the clue to the password which is “sum one and two when sorted they are true”.  Decode the clue from the test case and help Iron Man open the safe.

---
+++ Sort/16.py
@@ -0,0 +1,35 @@
+# Question:-
+'''
+Iron Man wants to extract an infinity stone from a safe. The safe is protected by a password and Iron Man knows the clue to the password which is “sum one and two when sorted they are true”. 
+Decode the clue from the test case and help Iron Man open the safe.
+
+ 
+Input Description:
+Size of the array followed by the elements of the array
+
+Output Description:
+Sum of 2 specific elements in the array.
+
+
+Sample Input :
+5
+9 8 3 2 1
+
+Sample Output :
+3
+'''
+
+
+
+# SOLUTION:-
+size = int(input())
+array = list(map(int, input().split()))
+
+# Sort the array
+sorted_array = sorted(array)
+
+# Find the sum of the first and second elements
+sum_of_elements = sorted_array[0] + sorted_array[1]
+
+# Print the result
+print(sum_of_elements)
