7451a74014a24d6605428e3210faf89a9b4151bd
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-20 17:40:38 +0530

    Given 2 numbers N and M add both the numbers and check whether the sum is odd or even.

---
+++ Maths/52.py
@@ -0,0 +1,20 @@
+# Question:-
+'''
+Given 2 numbers N and M add both the numbers and check whether the sum is odd or even.
+
+Sample Testcase :
+INPUT
+9 2
+
+OUTPUT
+odd
+'''
+
+
+# Solution:-
+n,m = input().split()
+a = int(n)+int(m)
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
