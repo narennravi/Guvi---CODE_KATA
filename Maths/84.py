8d227622be41bb0528ef498b92fc521fed2f269e
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-20 17:54:27 +0530

    Given 3 numbers A,B,C process and print 'yes' if they can form the sides of a triangle otherwise print 'no'.y

---
+++ Maths/84.py
@@ -0,0 +1,21 @@
+# Question:-
+'''
+Given 3 numbers A,B,C process and print 'yes' if they can form the sides of a triangle otherwise print 'no'.
+Input Size : A,B,C <= 100000
+
+
+Sample Testcase :
+INPUT
+3 4 5
+
+OUTPUT
+yes
+'''
+
+
+# Solution:-
+A,B,C=(int(no) for no in input().split())
+if ((A*A)+(B*B)==C*C):
+  print('yes')
+else:
+  print('no')
